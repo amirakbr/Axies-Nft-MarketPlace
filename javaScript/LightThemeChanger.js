d6d4dcc0f843b5750d5bd4015d10cec20b0e5a95
Amir <amir.akbrzadh3@gmail.com>
2022-10-30 00:58:24 +0330

    fix all bugs

--- javaScript/LightThemeChanger.js
+++ javaScript/LightThemeChanger.js
@@ -1,5 +1,5 @@
 const lighttheme = document.querySelectorAll('.header__lightTheme') ; 
-const darktheme = document.querySelector('.header__blackTheme') ; 
+const darktheme = document.querySelectorAll('.header__blackTheme') ; 
 const header = document.querySelector('.header') ; 
 const hero = document.querySelector('.hero') ; 
 const swipper = document.querySelector('.swiper') ; 
@@ -12,6 +12,7 @@ const todapick = document.querySelector('.todayPicksZ') ;
 const footer = document.querySelector('.footer') ; 
 const menu = document.querySelectorAll('.menu') ; 
 const menu1 = document.querySelectorAll('.menu1') ; 
+const hamburgerIcon = document.querySelector('.hamburger__container') ; 
 lighttheme.forEach(element => {
     element.addEventListener('click' , function(){
         if(header.classList.contains('darkHeader')){
@@ -44,43 +45,58 @@ lighttheme.forEach(element => {
                 elemm.classList.add('lightmenu') ;
             });
             }
+            hamburgerIcon.classList.add('lightHamburger') ; 
             document.querySelector('body').classList.add('lightbody') ; 
             document.querySelector('body').classList.remove('darkbody') ; 
             
     })
 });
-darktheme.addEventListener('click' ,function(w){
-    header.classList.add('darkHeader') ; 
-    header.classList.remove('lightHeader') ; 
-    main.classList.add('maindark') ; 
-    main.classList.remove('mainlight')
-    hero.classList.add('herodark') ; 
-    hero.classList.remove('herolight')  ;
-    swiperheader.classList.add('swiper__header--dark') ; 
-    swiperheader.classList.remove('swiper__header--light') ; 
-    swipper.classList.add('swiper__container--dark') ; 
-    swipper.classList.remove('swiper__container--light') ; 
-    feature.classList.add('feature__dark') ; 
-    feature.classList.remove('feature__light') ; 
-    popular.classList.add('popular__dark') ; 
-    popular.classList.remove('popular__light') ; 
-    topseller.classList.add('top__seller--dark') ;
-    topseller.classList.remove('top__seller--light') ;
-    todapick.classList.add('today__picks--dark') ;
-    todapick.classList.remove('today__picks--light') ;  
-    footer.classList.add('footer--dark') ; 
-    footer.classList.remove('footer--light') ;
-    menu.forEach(elem => {
-        elem.classList.add('darkmenu') ;
-        elem.classList.remove('lightmenu') ;
-    });
-    menu1.forEach(elemm => {
-        elemm.classList.add('darkmenu') ;
-        elemm.classList.remove('lightmenu') ;
-    });
-    document.querySelector('body').classList.remove('lightbody') ; 
-    document.querySelector('body').classList.add('darkbody') ; 
+darktheme.forEach((e)=>{
+    e.addEventListener('click',()=>{
+        if(header.classList.contains('lightHeader')){
+            header.classList.add('darkHeader') ; 
+            header.classList.remove('lightHeader') ; 
+            main.classList.add('maindark') ; 
+            main.classList.remove('mainlight')
+            hero.classList.add('herodark') ; 
+            hero.classList.remove('herolight')  ;
+            swiperheader.classList.add('swiper__header--dark') ; 
+            swiperheader.classList.remove('swiper__header--light') ; 
+            swipper.classList.add('swiper__container--dark') ; 
+            swipper.classList.remove('swiper__container--light') ; 
+            feature.classList.add('feature__dark') ; 
+            feature.classList.remove('feature__light') ; 
+            popular.classList.add('popular__dark') ; 
+            popular.classList.remove('popular__light') ; 
+            topseller.classList.add('top__seller--dark') ;
+            topseller.classList.remove('top__seller--light') ;
+            todapick.classList.add('today__picks--dark') ;
+            todapick.classList.remove('today__picks--light') ;  
+            footer.classList.add('footer--dark') ; 
+            footer.classList.remove('footer--light') ;
+            menu.forEach(elem => {
+                elem.classList.add('darkmenu') ;
+                elem.classList.remove('lightmenu') ;
+            });
+            menu1.forEach(elemm => {
+                elemm.classList.add('darkmenu') ;
+                elemm.classList.remove('lightmenu') ;
+            });
+            hamburgerIcon.classList.remove('lightHamburger') ; 
+            document.querySelector('body').classList.remove('lightbody') ; 
+            document.querySelector('body').classList.add('darkbody') ;       
+          }
+    })  
 })
+/*
+
+darktheme.forEach(element => {
+    element.addEventListener('click' , function(){
+        
+    })
+});
+*/
+
 /*
     else {
      
